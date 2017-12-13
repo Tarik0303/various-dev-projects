97149bcccf5257d6a614ff457e153b437010b40b
Tarik Terzimehic <terzimehic@fortiss.org>
2017-12-13 18:20:21 +0100

    add emf tutorial

---
+++ EMF/EMF_tutorial/org.eclipse.example.bowlingmodel.tests/src/bowling/tests/GameTest.java
@@ -0,0 +1,142 @@
+/**
+ */
+package bowling.tests;
+
+import javax.tools.Diagnostic;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import bowling.BowlingFactory;
+import bowling.BowlingPackage;
+import bowling.Game;
+import bowling.League;
+import bowling.Matchup;
+import bowling.Player;
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Game</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class GameTest extends TestCase {
+
+	/**
+	 * The fixture for this Game test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected Game fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(GameTest.class);
+	}
+
+	/**
+	 * Constructs a new Game test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public GameTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Game test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(Game fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Game test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected Game getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(BowlingFactory.eINSTANCE.createGame());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+	public void testMatchupGameRef() {
+		Matchup matchup = BowlingFactory.eINSTANCE.createMatchup();
+		Game game = BowlingFactory.eINSTANCE.createGame();
+		matchup.getGame().add(game);
+		assertEquals(game.getMatchup(), matchup);
+	}
+	/*The super class EObjects offers many methods to access an entity in 
+	 * a more generic way. For example, we will test the containment between 
+	 * Matchup and Game by accessing the EContainer instead of the getMatchup() method.*/
+	//	public void testMatchupGameRef() {
+	//		Matchup matchup = BowlingFactory.eINSTANCE.createMatchup();
+	//		Game game = BowlingFactory.eINSTANCE.createGame();
+	//		matchup.getGames().add(game);
+	//		assertEquals(game.eContainer(), matchup);
+	//	}
+
+	public void testReflection() {
+		EObject eObject = BowlingFactory.eINSTANCE.createPlayer();
+		eObject.eSet(BowlingPackage.eINSTANCE.getPlayer_Name(), "Jonas");
+		Player player = (Player) eObject;
+		assertEquals("Jonas", player.getName());
+	}
+
+	public void testReflectiveInformation() {
+		League league = BowlingFactory.eINSTANCE.createLeague();
+		assertTrue(league.eClass().getEAllReferences().get(0).isMany());
+		assertTrue(BowlingPackage.eINSTANCE.getLeague_Players().isMany());
+	}
+
+	public void testValidation() {
+		Matchup matchup = BowlingFactory.eINSTANCE.createMatchup();
+		matchup.getGame().add(BowlingFactory.eINSTANCE.createGame());
+		org.eclipse.emf.common.util.Diagnostic validate = Diagnostician.INSTANCE.validate(matchup);
+		assertEquals(org.eclipse.emf.common.util.Diagnostic.ERROR, validate.getSeverity());
+	}
+
+	public void testCopy() {
+		Player player = BowlingFactory.eINSTANCE.createPlayer();
+		player.setName("Jonas");
+		Player copy = EcoreUtil.copy(player);
+		assertNotSame(player, copy);
+		assertEquals(player.getName(), copy.getName());
+	}
+
+} //GameTest
